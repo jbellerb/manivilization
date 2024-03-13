8274b60e15ba919038c69820d9394324bca17a6c
jae beller <foss@jae.zone>
2024-03-12 23:53:04 -0400

    editor for checkbox questions

--- components/TextInput.tsx
+++ components/TextInput.tsx
@@ -1,8 +1,11 @@
+import { JSX } from "preact";
+
 import classnames from "../utils/classnames.ts";
 
 type Props = {
   name: string;
-  label: string;
+  label?: string;
+  onChange?: (event: JSX.TargetedEvent<HTMLInputElement, Event>) => void;
   value?: string;
   placeholder?: string;
   required?: boolean;
@@ -12,23 +15,30 @@ type Props = {
 export default function TextInput(props: Props) {
   return (
     <div
-      class={classnames("grow relative pt-4 text-lg z-0", props.class ?? "")}
+      class={classnames(
+        "grow relative text-lg",
+        { "pt-4 z-0": Boolean(props.label) },
+        props.class ?? "",
+      )}
     >
       <input
         type="text"
         name={props.name}
         id={`input-${props.name}`}
         class="block w-full py-1 bg-transparent border-b-2 border-gray-600 hover:border-gray-500 focus-visible:border-white transition-border-color peer"
+        onChange={props.onChange}
         value={props.value}
         placeholder={props.placeholder}
         required={props.required ?? false}
       />
-      <label
-        for={`input-${props.name}`}
-        class="absolute text-sm text-gray-400 peer-focus-visible:text-gray-600 font-semibold origin-[0] top-0 transition-color -z-10"
-      >
-        {props.label}
-      </label>
+      {props.label && (
+        <label
+          for={`input-${props.name}`}
+          class="absolute text-sm text-gray-400 peer-focus-visible:text-gray-600 font-semibold origin-[0] top-0 transition-color -z-10"
+        >
+          {props.label}
+        </label>
+      )}
     </div>
   );
 }
--- islands/QuestionEditor.tsx
+++ islands/QuestionEditor.tsx
@@ -8,7 +8,7 @@ import { Form, Question } from "../utils/form.ts";
 type IconButtonProps = {
   label: string;
   class: string;
-  onClick: () => void;
+  onClick?: () => void;
 };
 
 function IconButton(props: IconButtonProps) {
@@ -26,41 +26,94 @@ function IconButton(props: IconButtonProps) {
   );
 }
 
+type OptionsEditorProps = {
+  name: string;
+  options: string[];
+  onChange?: (options: string[]) => void;
+};
+
+function OptionsEditor(props: OptionsEditorProps) {
+  const addOption = (option: string) =>
+    props.onChange && props.onChange([...props.options, option]);
+  const deleteOption = (idx: number) =>
+    props.options.length > 1 && props.onChange &&
+    props.onChange(props.options.toSpliced(idx, 1));
+  const updateOption = (option: string, idx: number) =>
+    props.onChange && props.onChange(props.options.toSpliced(idx, 1, option));
+
+  return (
+    <div class="flex">
+      <IconButton
+        label="Add an option"
+        class="mt-auto after:i-mdi-plus"
+        onClick={() => addOption("New Option")}
+      />
+      <div class="flex flex-col w-full ml-2">
+        <span class="text-sm font-semibold text-gray-400">Options</span>
+        <ul class="-mt-1">
+          {props.options.map((option, idx) => (
+            <li class="flex">
+              <TextInput
+                name={`${props.name}-option-${idx}`}
+                value={option}
+                onChange={(e) => updateOption(e.currentTarget.value, idx)}
+              />
+              <IconButton
+                label="Delete option"
+                class="after:i-mdi-close"
+                onClick={() => deleteOption(idx)}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
 export type Props = {
   questions: Form["questions"];
 };
 
 export default function FormEditor(props: Props) {
-  const test = [
-    { type: "text", name: "test1", label: "Label?" },
-    { type: "checkbox", name: "test2", options: ["a", "b", "c"] },
-  ] satisfies Question[];
-  const questions = useSignal(test ?? []);
+  const questions = useSignal(props.questions ?? []);
 
   const addQuestion = (question: Question) =>
     questions.value = [...questions.value, question];
   const deleteQuestion = (idx: number) =>
     questions.value = questions.value.toSpliced(idx, 1);
+  const updateQuestion = (question: Question, idx: number) =>
+    questions.value = questions.value.toSpliced(idx, 1, question);
   const swapQuestions = (a: number, b: number) => {
     const copy = [...questions.value];
     [copy[a], copy[b]] = [copy[b], copy[a]];
     questions.value = copy;
   };
-  const changeType = (idx: number, type: string) =>
-    questions.value = questions.value.map((question, i) =>
-      (idx === i)
-        ? (type === "text"
-          ? { type: "text", name: question.name, label: "" }
-          : { type: "checkbox", name: question.name, options: [""] })
-        : question
-    );
+
+  // This cast retains associated data with the previous type in case the user
+  // changes it back. This is safe as only visible form elements are submitted.
+  const changeType = (question: Question, type: Question["type"]) => ({
+    ...question,
+    type,
+    ...(type === "text"
+      ? { label: "label" in question ? question.label : "" }
+      : type === "checkbox"
+      ? { options: "options" in question ? question.options : ["New Option"] }
+      : undefined),
+  } as Question);
 
   return (
-    <>
+    <div>
       <ul class="space-y-6">
         {questions.value.map((question: Question, idx: number) => (
           <li class="flex" key={question.name}>
             <div class="flex flex-col mr-2 space-y-2">
+              <IconButton
+                label="Delete question"
+                class="after:i-mdi-close"
+                onClick={() =>
+                  deleteQuestion(idx)}
+              />
               <IconButton
                 label="Move question up"
                 class="after:i-mdi-arrow-up"
@@ -76,19 +129,20 @@ export default function FormEditor(props: Props) {
                     idx + 1 < questions.value.length ? idx + 1 : idx,
                   )}
               />
-              <IconButton
-                label="Delete question"
-                class="after:i-mdi-close"
-                onClick={() =>
-                  deleteQuestion(idx)}
-              />
             </div>
             <div class="flex grow flex-col space-y-2">
               <div class="flex items-center space-x-4">
                 <Select
                   name={`question-${idx}-type`}
                   label="Type"
-                  onChange={(e) => changeType(idx, e.currentTarget.value)}
+                  onChange={(e) =>
+                    updateQuestion(
+                      changeType(
+                        question,
+                        e.currentTarget.value as Question["type"],
+                      ),
+                      idx,
+                    )}
                   value={question.type}
                 >
                   <option value="text">Textbox</option>
@@ -101,13 +155,37 @@ export default function FormEditor(props: Props) {
                 />
               </div>
               {(question.type === "text")
-                ? <TextInput name={`question-${idx}-label`} label="Question" />
+                ? (
+                  <TextInput
+                    name={`question-${idx}-label`}
+                    label="Question"
+                    value={question.label}
+                    onChange={(e) =>
+                      updateQuestion({
+                        ...question,
+                        label: e.currentTarget.value,
+                      }, idx)}
+                  />
+                )
+                : (question.type === "checkbox")
+                ? (
+                  <OptionsEditor
+                    name={`question-${idx}`}
+                    options={question.options}
+                    onChange={(options) =>
+                      updateQuestion({ ...question, options }, idx)}
+                  />
+                )
                 : undefined}
             </div>
           </li>
         ))}
       </ul>
-      <div class="flex justify-end">
+      <div
+        class={classnames("flex justify-end", {
+          "mt-4": questions.value.length > 0,
+        })}
+      >
         <button
           type="button"
           class="p-2 border-2 border-gray-600 hover:border-gray-500 focus-visible:border-white active:border-white rounded-full transition-border-color after:i-mdi-plus"
@@ -119,6 +197,6 @@ export default function FormEditor(props: Props) {
         >
         </button>
       </div>
-    </>
+    </div>
   );
 }
