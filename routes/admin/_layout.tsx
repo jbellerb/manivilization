388f31ce1474e3c5d6a2330c8606f3b0615cb4b1
jae beller <foss@jae.zone>
2024-03-21 01:11:08 -0400

    fix broken underline for the create form buttom

--- routes/admin/_layout.tsx
+++ routes/admin/_layout.tsx
@@ -19,7 +19,7 @@ export default defineLayout<AdminState>(async (_req, ctx) => {
         class="flex flex-col w-48 h-full px-3 py-4 border-r border-black"
         aria-label="Sidebar"
       >
-        <nav class="h-full pl-2 overflow-y-auto">
+        <nav class="h-full ml-2 overflow-y-auto">
           <ul class="space-y-4 flex flex-col items-start">
             {forms.map(({ id, name }) => (
               <li>
@@ -39,12 +39,14 @@ export default defineLayout<AdminState>(async (_req, ctx) => {
           </ul>
         </nav>
         <hr class="my-4 border-black" />
-        <a
-          href="/admin/forms/new"
-          class="block pl-2 text-browser-blue border-b border-transparent hover:border-browser-blue italic"
-        >
-          Create new form
-        </a>
+        <div role="presentation" class="ml-2">
+          <a
+            href="/admin/forms/new"
+            class="text-browser-blue border-b border-transparent hover:border-browser-blue italic"
+          >
+            Create new form
+          </a>
+        </div>
       </aside>
       <div class="flex flex-col grow h-full overflow-y-auto">
         <ctx.Component />
